86f6c603af3cd3bb34f88e0d2a1400c86524b855
artsamoilov <art@htmlacademy.ru>
2020-08-22 23:07:41 +0300

    Добавляет код для 1 кнопки

--- main.js
+++ main.js
@@ -1,18 +1,59 @@
-let phoneNumber = prompt('Введите 12-ти значный номер телефона');
+const $btn = document.getElementById('btn-kick');
 
-function formattedPhone(phone) {
-  let newString = '';
-  for (let i = 0; i < phone.length; i++) {
-    if (i == 2) {
-      newString += ' (';
-    } else if (i == 5) {
-      newString += ') ';
-    } else if (i == 8 || i == 10) {
-      newString += '-';
-    }
-    newString += phone.charAt(i);
-  }
-  return newString;
+const character = {
+    name: 'Pikachu',
+    defaultHP: 100,
+    damageHP: 100,
+    elHP: document.getElementById('health-character'),
+    elProgressbar: document.getElementById('progressbar-character'),
 }
 
-alert(formattedPhone(phoneNumber));
+const enemy = {
+    name: 'Charmander',
+    defaultHP: 100,
+    damageHP: 100,
+    elHP: document.getElementById('health-enemy'),
+    elProgressbar: document.getElementById('progressbar-enemy'),
+}
+
+$btn.addEventListener('click', function() {
+    console.log('Kick');
+    changeHP(random(20), character);
+    changeHP(random(20), enemy);
+});
+
+function init() {
+    console.log('Start game!');
+    renderHP(character);
+    renderHP(enemy);
+}
+
+function renderHP(person) {
+    renderHPLife(person);
+    renderProgressbarHP(person);
+}
+
+function renderHPLife(person) {
+    person.elHP.innerText = person.damageHP + ' ' + person.defaultHP;
+}
+
+function renderProgressbarHP(person) {
+    person.elProgressbar.style.width = person.damageHP + '%';
+}
+
+function changeHP(count, person) {
+    if (person.damageHP < count) {
+        person.damageHP = 0;
+        alert('Бедный ' + person.name + ' проиграл бой!');
+        $btn.disabled = true;
+    } else {
+        person.damageHP -= count;
+    }
+    renderHP(person);
+}
+
+function random(num) {
+    return Math.ceil(Math.random() * num);
+}
+
+init();
